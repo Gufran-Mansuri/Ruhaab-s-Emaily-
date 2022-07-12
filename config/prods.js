c14f40c915c06ed1dec47014310a1ed75b046223
Gufran <gufranmansuri15@gmail.com>
2022-07-12 16:28:36 -0400

    final commit

--- config/prods.js
+++ config/prods.js
@@ -9,7 +9,3 @@ module.exports = {
     redirectDomain: process.env.REDIRECT_DOMAIN
 };
 
-//mongo prod pass - 5dPt3nm8JsXqYoiL
-//mongo prod link: mongodb+srv://Gufranmansuri:5dPt3nm8JsXqYoiL@cluster0.ci6nrnm.mongodb.net/?retryWrites=true&w=majority
-//google client prod id: 572604267928-jbqbj4tglfg2cubi5bj5gur67091292t.apps.googleusercontent.com
-//google client prod secret: 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
